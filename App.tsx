1913f1002b9fccb8bb8a662a7e93052ecbb0d3dc
Manoel Duran <manoel.duran@hotmail.com>
2021-10-06 17:42:54 -0300

    context criado

--- App.tsx
+++ App.tsx
@@ -14,7 +14,8 @@ import theme from './src/global/styles/theme';
 import { ThemeProvider } from 'styled-components'
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes/app.routes';
-
+import { SignIn } from './src/screens/SignIn';
+import { AuthProvider } from './src/hooks/auth';
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -28,7 +29,9 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
-        <AppRoutes />
+        <AuthProvider>
+          <SignIn />
+        </AuthProvider>
       </NavigationContainer>
     </ThemeProvider>
   )
